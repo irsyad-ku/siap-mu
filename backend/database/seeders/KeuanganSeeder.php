2340918a20dadf5bbb5076ba1823014356363445
Irsyad Khoerul Umam <irsyadbanjar05@gmail.com>
2026-05-22 19:20:02 +0700

    Update seeder dates to match 2026 application context and refresh database

--- backend/database/seeders/KeuanganSeeder.php
+++ backend/database/seeders/KeuanganSeeder.php
@@ -10,20 +10,20 @@ class KeuanganSeeder extends Seeder
     public function run(): void
     {
         $transaksi = [
-            ['jenis' => 'pemasukan', 'kategori' => 'Infaq Jumat', 'jumlah' => 3500000, 'keterangan' => 'Infaq Shalat Jumat Minggu 1', 'tanggal' => '2025-05-02', 'id_user' => 1],
-            ['jenis' => 'pemasukan', 'kategori' => 'Infaq Jumat', 'jumlah' => 4200000, 'keterangan' => 'Infaq Shalat Jumat Minggu 2', 'tanggal' => '2025-05-09', 'id_user' => 1],
-            ['jenis' => 'pemasukan', 'kategori' => 'Donasi', 'jumlah' => 10000000, 'keterangan' => 'Donasi H. Ahmad untuk renovasi', 'tanggal' => '2025-05-10', 'id_user' => 2],
-            ['jenis' => 'pemasukan', 'kategori' => 'Zakat', 'jumlah' => 15000000, 'keterangan' => 'Pengumpulan zakat fitrah', 'tanggal' => '2025-03-28', 'id_user' => 1],
-            ['jenis' => 'pemasukan', 'kategori' => 'Infaq Jumat', 'jumlah' => 3800000, 'keterangan' => 'Infaq Shalat Jumat Minggu 3', 'tanggal' => '2025-05-16', 'id_user' => 1],
-            ['jenis' => 'pemasukan', 'kategori' => 'Donasi', 'jumlah' => 5000000, 'keterangan' => 'Donasi keluarga Bpk. Hasan', 'tanggal' => '2025-04-15', 'id_user' => 2],
-            ['jenis' => 'pemasukan', 'kategori' => 'Kotak Amal', 'jumlah' => 2300000, 'keterangan' => 'Kotak amal bulanan April', 'tanggal' => '2025-04-30', 'id_user' => 1],
-            ['jenis' => 'pengeluaran', 'kategori' => 'Listrik & Air', 'jumlah' => 1500000, 'keterangan' => 'Pembayaran listrik bulan Mei', 'tanggal' => '2025-05-05', 'id_user' => 1],
-            ['jenis' => 'pengeluaran', 'kategori' => 'Kebersihan', 'jumlah' => 800000, 'keterangan' => 'Gaji petugas kebersihan Mei', 'tanggal' => '2025-05-01', 'id_user' => 2],
-            ['jenis' => 'pengeluaran', 'kategori' => 'Perbaikan', 'jumlah' => 3500000, 'keterangan' => 'Perbaikan atap bocor sisi timur', 'tanggal' => '2025-04-20', 'id_user' => 1],
-            ['jenis' => 'pengeluaran', 'kategori' => 'Kegiatan', 'jumlah' => 2000000, 'keterangan' => 'Konsumsi buka puasa bersama', 'tanggal' => '2025-03-25', 'id_user' => 2],
-            ['jenis' => 'pengeluaran', 'kategori' => 'ATK', 'jumlah' => 350000, 'keterangan' => 'Pembelian alat tulis kantor', 'tanggal' => '2025-05-12', 'id_user' => 1],
-            ['jenis' => 'pengeluaran', 'kategori' => 'Honor Ustadz', 'jumlah' => 2500000, 'keterangan' => 'Honor penceramah bulan Mei', 'tanggal' => '2025-05-15', 'id_user' => 2],
-            ['jenis' => 'pengeluaran', 'kategori' => 'Internet', 'jumlah' => 500000, 'keterangan' => 'Pembayaran wifi bulan Mei', 'tanggal' => '2025-05-03', 'id_user' => 1],
+            ['jenis' => 'pemasukan', 'kategori' => 'Infaq Jumat', 'jumlah' => 3500000, 'keterangan' => 'Infaq Shalat Jumat Minggu 1', 'tanggal' => '2026-05-02', 'id_user' => 1],
+            ['jenis' => 'pemasukan', 'kategori' => 'Infaq Jumat', 'jumlah' => 4200000, 'keterangan' => 'Infaq Shalat Jumat Minggu 2', 'tanggal' => '2026-05-09', 'id_user' => 1],
+            ['jenis' => 'pemasukan', 'kategori' => 'Donasi', 'jumlah' => 10000000, 'keterangan' => 'Donasi H. Ahmad untuk renovasi', 'tanggal' => '2026-05-10', 'id_user' => 2],
+            ['jenis' => 'pemasukan', 'kategori' => 'Zakat', 'jumlah' => 15000000, 'keterangan' => 'Pengumpulan zakat fitrah', 'tanggal' => '2026-03-28', 'id_user' => 1],
+            ['jenis' => 'pemasukan', 'kategori' => 'Infaq Jumat', 'jumlah' => 3800000, 'keterangan' => 'Infaq Shalat Jumat Minggu 3', 'tanggal' => '2026-05-16', 'id_user' => 1],
+            ['jenis' => 'pemasukan', 'kategori' => 'Donasi', 'jumlah' => 5000000, 'keterangan' => 'Donasi keluarga Bpk. Hasan', 'tanggal' => '2026-04-15', 'id_user' => 2],
+            ['jenis' => 'pemasukan', 'kategori' => 'Kotak Amal', 'jumlah' => 2300000, 'keterangan' => 'Kotak amal bulanan April', 'tanggal' => '2026-04-30', 'id_user' => 1],
+            ['jenis' => 'pengeluaran', 'kategori' => 'Listrik & Air', 'jumlah' => 1500000, 'keterangan' => 'Pembayaran listrik bulan Mei', 'tanggal' => '2026-05-05', 'id_user' => 1],
+            ['jenis' => 'pengeluaran', 'kategori' => 'Kebersihan', 'jumlah' => 800000, 'keterangan' => 'Gaji petugas kebersihan Mei', 'tanggal' => '2026-05-01', 'id_user' => 2],
+            ['jenis' => 'pengeluaran', 'kategori' => 'Perbaikan', 'jumlah' => 3500000, 'keterangan' => 'Perbaikan atap bocor sisi timur', 'tanggal' => '2026-04-20', 'id_user' => 1],
+            ['jenis' => 'pengeluaran', 'kategori' => 'Kegiatan', 'jumlah' => 2000000, 'keterangan' => 'Konsumsi buka puasa bersama', 'tanggal' => '2026-03-25', 'id_user' => 2],
+            ['jenis' => 'pengeluaran', 'kategori' => 'ATK', 'jumlah' => 350000, 'keterangan' => 'Pembelian alat tulis kantor', 'tanggal' => '2026-05-12', 'id_user' => 1],
+            ['jenis' => 'pengeluaran', 'kategori' => 'Honor Ustadz', 'jumlah' => 2500000, 'keterangan' => 'Honor penceramah bulan Mei', 'tanggal' => '2026-05-15', 'id_user' => 2],
+            ['jenis' => 'pengeluaran', 'kategori' => 'Internet', 'jumlah' => 500000, 'keterangan' => 'Pembayaran wifi bulan Mei', 'tanggal' => '2026-05-03', 'id_user' => 1],
         ];
 
         foreach ($transaksi as $t) {
--- backend/database/seeders/PengumumanSeeder.php
+++ backend/database/seeders/PengumumanSeeder.php
@@ -10,10 +10,10 @@ class PengumumanSeeder extends Seeder
     public function run(): void
     {
         $pengumuman = [
-            ['judul' => 'Jadwal Imam dan Khatib Bulan Juni 2025', 'isi' => 'Berikut jadwal imam dan khatib shalat Jumat untuk bulan Juni 2025. Mohon para imam dan khatib mempersiapkan diri. Jadwal lengkap dapat dilihat di papan pengumuman masjid.', 'tanggal' => '2025-05-20', 'is_aktif' => true, 'id_user' => 1],
-            ['judul' => 'Pendaftaran TPA Tahun Ajaran Baru', 'isi' => 'Dibuka pendaftaran peserta TPA tahun ajaran baru 2025/2026 untuk anak usia 5-12 tahun. Pendaftaran dibuka mulai 1 Juni - 30 Juni 2025 di kantor DKM. Biaya pendaftaran Rp 50.000. Kuota terbatas 40 anak.', 'tanggal' => '2025-05-18', 'is_aktif' => true, 'id_user' => 1],
-            ['judul' => 'Pemberitahuan Renovasi Toilet Wudhu', 'isi' => 'Diberitahukan kepada jamaah bahwa toilet dan tempat wudhu sisi barat akan direnovasi mulai tanggal 1-15 Juni 2025. Selama renovasi, jamaah dapat menggunakan toilet dan tempat wudhu sisi timur. Mohon maaf atas ketidaknyamanannya.', 'tanggal' => '2025-05-15', 'is_aktif' => true, 'id_user' => 2],
-            ['judul' => 'Laporan Keuangan Bulan April 2025', 'isi' => 'Laporan keuangan masjid bulan April 2025 sudah tersedia. Total pemasukan Rp 43.800.000 dan total pengeluaran Rp 11.150.000. Laporan lengkap dapat dilihat di papan pengumuman atau meminta salinan di kantor DKM.', 'tanggal' => '2025-05-01', 'is_aktif' => true, 'id_user' => 1],
+            ['judul' => 'Jadwal Imam dan Khatib Bulan Juni 2026', 'isi' => 'Berikut jadwal imam dan khatib shalat Jumat untuk bulan Juni 2026. Mohon para imam dan khatib mempersiapkan diri. Jadwal lengkap dapat dilihat di papan pengumuman masjid.', 'tanggal' => '2026-05-20', 'is_aktif' => true, 'id_user' => 1],
+            ['judul' => 'Pendaftaran TPA Tahun Ajaran Baru', 'isi' => 'Dibuka pendaftaran peserta TPA tahun ajaran baru 2026/2027 untuk anak usia 5-12 tahun. Pendaftaran dibuka mulai 1 Juni - 30 Juni 2026 di kantor DKM. Biaya pendaftaran Rp 50.000. Kuota terbatas 40 anak.', 'tanggal' => '2026-05-18', 'is_aktif' => true, 'id_user' => 1],
+            ['judul' => 'Pemberitahuan Renovasi Toilet Wudhu', 'isi' => 'Diberitahukan kepada jamaah bahwa toilet dan tempat wudhu sisi barat akan direnovasi mulai tanggal 1-15 Juni 2026. Selama renovasi, jamaah dapat menggunakan toilet dan tempat wudhu sisi timur. Mohon maaf atas ketidaknyamanannya.', 'tanggal' => '2026-05-15', 'is_aktif' => true, 'id_user' => 2],
+            ['judul' => 'Laporan Keuangan Bulan April 2026', 'isi' => 'Laporan keuangan masjid bulan April 2026 sudah tersedia. Total pemasukan Rp 43.800.000 dan total pengeluaran Rp 11.150.000. Laporan lengkap dapat dilihat di papan pengumuman atau meminta salinan di kantor DKM.', 'tanggal' => '2026-05-01', 'is_aktif' => true, 'id_user' => 1],
         ];
 
         foreach ($pengumuman as $p) {
